88cf70c1c3e5dbecb309c017cc8bc59056dfe393
W. Gawroński <wgawronski.wg@gmail.com>
2023-03-29 13:42:13 +0200

    Added New view: RegisterView

---
+++ src/views/RegisterView/RegisterView.js
@@ -0,0 +1,14 @@
+import React from "react";
+import SiteTitle from "../../components/SiteTitle/SiteTitle";
+
+const RegisterView = () => {
+    return <>
+        <main>
+            <div className="container">
+                <SiteTitle>Register View</SiteTitle>
+            </div>
+        </main>
+    </>
+}
+
+export default RegisterView;
