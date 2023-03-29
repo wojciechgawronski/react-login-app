4e901c5bef7d398b0492c6c5fe6cc8811d4432d9
W. Gawroński <wgawronski.wg@gmail.com>
2023-03-29 11:12:44 +0200

    Refactor: Navigation: delete react-bootstrap package:

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,39 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Container from 'react-bootstrap/Container';
-import NavB from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 const Nav = () => {
     return <>
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="/">App</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <NavB className="me-auto">
-                        <NavB.Link><NavLink to='/'>Start</NavLink></NavB.Link>
-                        <NavB.Link><NavLink to='/login'>Login</NavLink></NavB.Link>
-                        <NavB.Link><NavLink to='/account'>Account</NavLink></NavB.Link>
-                    </NavB>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-
-        <br></br>
-        
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
+        <nav className="navbar navbar-expand-lg bg-light">
+            <div className="container-fluid">
                 <NavLink className="navbar-brand" to='/'>Start</NavLink>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item"><NavLink className="nav-link" to='/'>Start</NavLink></li>
-                        <li class="nav-item"><NavLink className="nav-link" to='/login'>Login</NavLink></li>
-                        <li class="nav-item"><NavLink className="nav-link" to='/account'>Account</NavLink></li>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item"><NavLink className="nav-link" to='/'>Start</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to='/login'>Login</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to='/account'>Account</NavLink></li>
                     </ul>
                 </div>
             </div>
