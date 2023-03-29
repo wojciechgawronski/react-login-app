688e8a0506fc232c483a5934bcebf70a663d9af3
W. Gawroński <wgawronski.wg@gmail.com>
2023-03-29 13:53:10 +0200

    Added RegisterView

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import AccountView from './views/AccountView/AccountView';
 import LoginView from './views/LoginView/LoginView';
+import RegisterView from './views/RegisterView/RegisterView';
 import StartView from './views/StartView/StartView';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
                 <Route path='/' Component={StartView}></Route>
                 <Route path='/login' Component={LoginView}></Route>
                 <Route path='/account' Component={AccountView}></Route>
+                <Route path='/register' Component={RegisterView}></Route>
             </Routes>
             <Footer/>
         </BrowserRouter>
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -13,6 +13,7 @@ const Nav = () => {
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item"><NavLink className="nav-link" to='/'>Start</NavLink></li>
                         <li className="nav-item"><NavLink className="nav-link" to='/login'>Login</NavLink></li>
+                        <li className="nav-item"><NavLink className="nav-link" to='/register'>Register</NavLink></li>
                         <li className="nav-item"><NavLink className="nav-link" to='/account'>Account</NavLink></li>
                     </ul>
                 </div>
