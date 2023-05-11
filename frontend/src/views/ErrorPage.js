cf1426f0f3606da89ab38d94c6705d6b13d56bd1
W. Gawroński <wgawronski.wg@gmail.com>
2023-05-11 11:34:04 +0200

    View: ErrorPage

---
+++ frontend/src/views/ErrorPage.js
@@ -0,0 +1,31 @@
+import { Link, useRouteError } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Header";
+
+const ErrorPage = () => {
+
+    const error = useRouteError();
+
+    let title = 'An error occured.';
+    let data = 'Something went wrong.'; 
+    let message = null;
+
+    if (error.status === 404) {
+        title = error.status;
+        message = error.statusText;
+        data = error.data;
+    }
+
+    
+    return <>
+        <Header/>
+            <div className="container">
+                <h2>{title} {message}</h2>
+                {data ?? <p>{data}</p>}
+                <p className="small mt-3"><Link to="/">Back to start page</Link></p>
+            </div>
+        <Footer/>
+    </>
+}
+
+export default ErrorPage;
