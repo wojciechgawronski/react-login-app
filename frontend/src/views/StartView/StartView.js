dc5132c5fbc28d42cba09096d0e808bb1458788b
W. Gawronski <wgawronski.wg@gmail.com>
2023-05-17 11:41:15 +0200

    To do list

--- frontend/src/views/StartView/StartView.js
+++ frontend/src/views/StartView/StartView.js
@@ -11,6 +11,26 @@ const StartView = () => {
                 <div className="row">
                     <div className="col">
                         <p className="lead">Simple app for register / login.</p>
+                        <article className="small">
+                            <p>----</p>
+                            <p>TO DO</p>
+                            <ol>
+                                <li>Auth Token</li>
+                                <li>DELETE BUTTON</li>
+                                <li>Pagination: Class Compoment - url change</li>
+                                <li>Pagination</li>
+                                <li>Flash messages</li>
+                                <li>Login, Register: CRUD, backend: SYMFONY API</li>
+                            </ol>
+                            <p>DONE:</p>
+                            <ul>
+                                <li>Pagination: Class Compoment</li>
+                                <li>Article: CRUD, backend</li>
+                                <li>Article: CRUD, frontend</li>
+                                <li>Login, Register: CRUD, frontend</li>
+                                <li>Login, Register: CRUD, backend: LARAVEL API</li>
+                            </ul>
+                        </article>
                     </div>
                 </div>
             </div>
